6e2519540817cf2d4c3825590a4c7faab293a7cf
Roya <royash-bb104@code.edu.az>
2025-04-13 15:47:33 -0700

    task

---
+++ Json/Json/Group.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public  class Group
+    {
+        public string Name { get; set; }
+        public List<Student> students = new List<Student>();
+    }
+}
---
+++ Json/Json/Program.cs
@@ -0,0 +1,51 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace Json
+{
+    public class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("\n1.Qruplara bax\n2.Qrup yarat\n3.Qrupu sil\n4.Qrupa kec");
+                Console.WriteLine("Choice:  ");
+                string choice=Console.ReadLine();
+                switch (choice) 
+                {
+                    case "1": ShowGrups();break;
+                    case "2": CreateGroup();break;
+                    case "3": RemoveGroup();break;
+                    case "4": EnterGroup();break;
+                    default: Console.WriteLine("yanlis  secim: ");break;
+
+                }
+            }
+        }
+        public void ShowGroups() 
+        {
+            foreach (var group in Groups)
+                Console.WriteLine($"{group.Name}");
+        }
+        public void CreateGroup() 
+        {
+            Console.WriteLine("Qrup adi:");
+            string name = Console.ReadLine();
+            Groups.Add(name,name);
+            Console.WriteLine("qrup yaradildi");
+        }
+        public void RemoveGroup()
+        {
+            Console.WriteLine("silinecek qrupun adi: ");
+            string name=Console.ReadLine();
+            Groups.Remove(name, name);
+            Console.WriteLine("qrup silindi:");
+        }
+        public void EnterGroup() 
+        {
+            Console.WriteLine("qrupun adi: ");
+            string name= Console.ReadLine();
+        }
+    }
+}
